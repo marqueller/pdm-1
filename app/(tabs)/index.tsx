a51a0d4daf81200893db870c6c29c6e269f7785c
nathan-rodriguess <nath4nfamilia123@gmail.com>
2024-07-04 09:19:28 -0300

    1° commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Image, StyleSheet, Dimensions } from 'react-native';
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/CAPA.jpg')}
+          style={styles.headerImage}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    width: windowWidth,  // Define a largura igual à largura da janela (tela)
+    height: 300,  // Altura fixa ou proporcional à largura, conforme necessário
+    resizeMode: 'cover',  // Ajusta a imagem para cobrir todo o espaço do container
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    paddingHorizontal: 20, // Adiciona padding horizontal se necessário
+  },
+});
