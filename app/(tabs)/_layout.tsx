d0a8bac8a1fd777178deb27b3938e079fece5cd8
Kauezn6 <owonkru66@gmail.com>
2024-07-08 09:29:05 -0300

    1° commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,16 +17,16 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Login',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="explorar"
         options={{
-          title: 'Explore',
+          title: 'Registro',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
